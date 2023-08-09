c0603f0a56683caff4099bf2a9c51ee3d1330978
almncarlo <cmalamani@up.edu.ph>
2023-08-09 19:36:28 +0800

    home page styling

--- src/home.js
+++ src/home.js
@@ -1,3 +1,58 @@
-const home = () => console.log('this is the home page');
+import { menu } from "./menu";
+import Icon from "./assets/hero.jpeg";
 
-export { home };
+const displayHome = (() => {
+    const content = document.querySelector('#content');
+    const header = document.createElement('header');
+    const navbar = document.createElement('nav');
+    const buttonList = document.createElement('ul');
+    const homeBox = document.createElement('li');
+    const menuBox = document.createElement('li');
+    const contactBox = document.createElement('li');
+    const homeB = document.createElement('a');
+    const menuB = document.createElement('a');
+    const contactB = document.createElement('a');
+    const hero = document.createElement('div');
+    const myIcon = new Image();
+    const landingText = document.createElement('h1');
+
+    myIcon.src = Icon;
+    hero.classList.add('hero');
+
+    // myIcon.classList.add('hero-img');
+
+    homeB.textContent = 'HOME';
+    menuB.textContent = 'MENU';
+    contactB.textContent = 'CONTACT';
+
+    homeBox.appendChild(homeB);
+    menuBox.appendChild(menuB);
+    contactBox.appendChild(contactB);
+
+    homeBox.classList.add('home');
+    menuBox.classList.add('menu');
+    contactBox.classList.add('contact');
+
+    buttonList.appendChild(homeBox);
+    buttonList.appendChild(menuBox);
+    buttonList.appendChild(contactBox);
+    
+    navbar.appendChild(buttonList);
+    navbar.classList.add('navbar');
+
+    header.appendChild(navbar);
+
+    // hero page stuff
+    landingText.textContent = "Chicken like never before";
+    landingText.classList.add('hero-text');
+    hero.appendChild(landingText);
+
+    // reset html when loading home page
+    content.innerHTML = '';
+
+    // add divs to content div
+    content.appendChild(header);
+    content.appendChild(hero);
+})();
+
+export { displayHome };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import { home } from './home';
+import './style.css';
+import { displayHome } from './home';
 import { contact } from './contact';
 import { menu } from './menu';
 
-const container = document.querySelector('#content');
+// const container = document.querySelector('#content');
 
-const homeB = document.createElement('button');
-homeB.textContent = 'click me for home';
-homeB.addEventListener('click', home);
+// const homeB = document.createElement('button');
+// homeB.textContent = 'click me for home';
+// homeB.addEventListener('click', displayHome);
 
-const contactB = document.createElement('button');
-contactB.textContent = 'click me for contact';
-contactB.addEventListener('click', contact);
+// const contactB = document.createElement('button');
+// contactB.textContent = 'click me for contact';
+// contactB.addEventListener('click', contact);
 
-const menuB = document.createElement('button');
-menuB.textContent = 'click me for menu';
-menuB.addEventListener('click', menu);
+// const menuB = document.createElement('button');
+// menuB.textContent = 'click me for menu';
+// menuB.addEventListener('click', menu);
 
-container.appendChild(homeB);
-container.appendChild(contactB);
-container.appendChild(menuB);
+// container.appendChild(homeB);
+// container.appendChild(contactB);
+// container.appendChild(menuB);
+
+displayHome();
