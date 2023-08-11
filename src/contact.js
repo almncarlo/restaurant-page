fbb0c3b7d375df61ab1e4b9b318eb554cdb67340
almncarlo <cmalamani@up.edu.ph>
2023-08-11 20:54:28 +0800

    contact page form added

--- src/contact.js
+++ src/contact.js
@@ -11,17 +11,59 @@ const displayContact = (() => {
     const side = document.createElement('div');
     side.classList.add('side');
 
-    const form = document.createElement('div');
-    form.classList.add('form-div');
+    const form_div = document.createElement('div');
+    form_div.classList.add('form-div');
 
     const heading = document.createElement('h2');
     heading.classList.add('form-header');
     heading.textContent = 'Get in touch with us';
+    form_div.appendChild(heading);
 
-    form.appendChild(heading);
+    // form section
+    const form = document.createElement('form');
+    const fname = document.createElement('input');
+    const l_fname = document.createElement('label');
+    const lname = document.createElement('input');
+    const l_lname = document.createElement('label');
+    const email = document.createElement('input');
+    const l_email = document.createElement('label');
+    const msg = document.createElement('input');
+    const l_msg = document.createElement('label');
+    const submit = document.createElement('button');
+
+    fname.id = 'fname';
+    l_fname.htmlFor = 'fname';
+    l_fname.textContent = 'First Name';
+
+    lname.id = 'lname';
+    l_lname.htmlFor = 'lname';
+    l_lname.textContent = 'Last Name';
+
+    email.id = 'email';
+    l_email.htmlFor = 'email';
+    l_email.textContent = 'Email Address';
+
+    msg.id = 'msg';
+    l_msg.htmlFor = 'msg';
+    l_msg.textContent = 'What would you like to tell us?';
+
+    submit.type = 'submit';
+    submit.textContent = 'Send message';
+
+    form.appendChild(l_fname);
+    form.appendChild(fname);
+    form.appendChild(l_lname);
+    form.appendChild(lname);
+    form.appendChild(l_email);
+    form.appendChild(email);
+    form.appendChild(l_msg);
+    form.appendChild(msg);
+    form.appendChild(submit);
+
+    form_div.appendChild(form);
 
     body.appendChild(side);
-    body.appendChild(form);
+    body.appendChild(form_div);
 
     content.appendChild(body);
 })();
